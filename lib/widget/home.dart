0943a4c793c61585f7b8fbd1760c392ddbde500a
amitgain1818 <amitgain1818@gmail.com>
2022-01-02 17:34:53 +0600

    Add

--- lib/widget/home.dart
+++ lib/widget/home.dart
@@ -28,27 +28,60 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.restaurant_menu),
-              SizedBox(width: 10),
-              Text('Food Recipe')
-            ],
-          ),
+      appBar: AppBar(
+        backgroundColor: Color(0xFFF44336),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.restaurant_menu),
+            SizedBox(width: 10),
+            Text('Food Recipe')
+          ],
         ),
-        body: _isLoading
-            ? Center(child: CircularProgressIndicator())
-            : ListView.builder(
-                itemCount: _recipes!.length,
-                itemBuilder: (context, index) {
-                  return RecipeCard(
-                      title: _recipes![index].name,
-                      cookTime: _recipes![index].totalTime,
-                      rating: _recipes![index].rating.toString(),
-                      thumbnailUrl: _recipes![index].images);
-                },
-              ));
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+              child: Text(
+                'Amit Gain',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _recipes!.length,
+              itemBuilder: (context, index) {
+                return RecipeCard(
+                    title: _recipes![index].name,
+                    cookTime: _recipes![index].totalTime,
+                    rating: _recipes![index].rating.toString(),
+                    thumbnailUrl: _recipes![index].images);
+              },
+            ),
+    );
   }
 }
